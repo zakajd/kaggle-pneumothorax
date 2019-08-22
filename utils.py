a0e11f4ae5d72703c2f176de42f5a48ed235958d
zakajd <djamilzak@gmail.com>
2019-08-23 02:59:34 +0300

    updated

--- utils.py
+++ utils.py
@@ -1,8 +1,37 @@
-# Collection of different usefull functions
+"""
+Collection of different usefull functions
+"""
+
 from skimage import exposure
 import pydicom
 import pytorch_tools
-# from pytorch_tools.utils import rle_decode
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import patches as patches
+
+def show_dicom_info(dataset):
+    print("Filename.........:", file_path)
+    print("Storage type.....:", dataset.SOPClassUID)
+    print()
+
+    pat_name = dataset.PatientName
+    display_name = pat_name.family_name + ", " + pat_name.given_name
+    print("Patient's name......:", display_name)
+    print("Patient id..........:", dataset.PatientID)
+    print("Patient's Age.......:", dataset.PatientAge)
+    print("Patient's Sex.......:", dataset.PatientSex)
+    print("Modality............:", dataset.Modality)
+    print("Body Part Examined..:", dataset.BodyPartExamined)
+    print("View Position.......:", dataset.ViewPosition)
+    
+    if 'PixelData' in dataset:
+        rows = int(dataset.Rows)
+        cols = int(dataset.Columns)
+        print("Image size.......: {rows:d} x {cols:d}, {size:d} bytes".format(
+            rows=rows, cols=cols, size=len(dataset.PixelData)))
+        if 'PixelSpacing' in dataset:
+            print("Pixel spacing....:", dataset.PixelSpacing)
 
 def dicom_to_dict(dicom_data, file_path, rles_df, encoded_pixels=True):
     """Parse DICOM dataset and returns a dictonary with relevant fields.
@@ -69,3 +98,63 @@ def rle_decode(rle_str, shape, fill_value=1, dtype=int, relative=False):
         for lo, hi in zip(starts, ends):
             mask[lo:hi] = 1
         return mask.reshape(shape[::-1]).T
+
+def bounding_box(img):
+    # return max and min of a mask to draw bounding box
+    rows = np.any(img, axis=1)
+    cols = np.any(img, axis=0)
+    rmin, rmax = np.where(rows)[0][[0, -1]]
+    cmin, cmax = np.where(cols)[0][[0, -1]]
+
+    return rmin, rmax, cmin, cmax
+
+def plot_with_mask_and_bbox(file_path, mask_encoded_list, figsize=(20,10)):    
+    """Plot Chest Xray image with mask(annotation or label) and without mask.
+
+    Args:
+        file_path (str): file path of the dicom data.
+        mask_encoded (numpy.ndarray): Pandas dataframe of the RLE.
+        
+    Returns:
+        plots the image with and without mask.
+    """
+    
+    pixel_array = pydicom.dcmread(file_path).pixel_array
+    print(np.max(pixel_array))
+    
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16, 16))
+    clahe_pixel_array = clahe.apply(pixel_array)
+    adapteq_pixel_array = exposure.equalize_adapthist(pixel_array, clip_limit=0.03)
+    
+    # use the masking function to decode RLE
+    mask_decoded_list = [rle_decode(mask_encoded, (1024, 1024), relative=True).T for mask_encoded in mask_encoded_list]
+    
+    fig, ax = plt.subplots(nrows=1, ncols=3, sharey=True, figsize=(20,10))
+    
+    # print out the xray
+    ax[0].imshow(pixel_array, cmap=plt.cm.bone)
+    # print the bounding box
+    for mask_decoded in mask_decoded_list:
+        # print out the annotated area
+        ax[0].imshow(mask_decoded, alpha=0.3, cmap="Reds")
+        rmin, rmax, cmin, cmax = bounding_box(mask_decoded)
+        bbox = patches.Rectangle((cmin,rmin),cmax-cmin,rmax-rmin,linewidth=1,edgecolor='r',facecolor='none')
+        ax[0].add_patch(bbox)
+    ax[0].set_title('With Mask')
+    
+    # plot image with clahe processing with just bounding box and no mask
+    ax[1].imshow(clahe_pixel_array, cmap=plt.cm.bone)
+    for mask_decoded in mask_decoded_list:
+        rmin, rmax, cmin, cmax = bounding_box(mask_decoded)
+        bbox = patches.Rectangle((cmin,rmin),cmax-cmin,rmax-rmin,linewidth=1,edgecolor='r',facecolor='none')
+        ax[1].add_patch(bbox)
+    ax[1].set_title('Without Mask - Clahe')
+    
+    # plot plain xray with just bounding box and no mask
+    ax[2].imshow(adapteq_pixel_array, cmap=plt.cm.bone)
+    for mask_decoded in mask_decoded_list:
+        rmin, rmax, cmin, cmax = bounding_box(mask_decoded)
+        bbox = patches.Rectangle((cmin,rmin),cmax-cmin,rmax-rmin,linewidth=1,edgecolor='r',facecolor='none')
+        ax[2].add_patch(bbox)
+    ax[2].set_title('Without Mask - Adapteq')
+    plt.show()
