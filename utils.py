ed1ea9ad8930ae84865f16a4a49185e476c0b939
zakajd <djamilzak@gmail.com>
2019-08-27 00:20:14 +0300

    essentials

--- utils.py
+++ utils.py
@@ -10,44 +10,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import patches as patches
 
-def show_dicom_info(dataset):
+def show_dicom_info():
     print("Filename.........:", file_path)
-    print("Storage type.....:", dataset.SOPClassUID)
+    dicom_data = pydicom.dcmread(file_path)
+    print("Instance UID......:", dicom_data.SOPInstanceUID)
+    print("Storage type.....:", dicom_data.SOPClassUID)
     print()
 
-    pat_name = dataset.PatientName
+    pat_name = dicom_data.PatientName
     display_name = pat_name.family_name + ", " + pat_name.given_name
     print("Patient's name......:", display_name)
-    print("Patient id..........:", dataset.PatientID)
-    print("Patient's Age.......:", dataset.PatientAge)
-    print("Patient's Sex.......:", dataset.PatientSex)
-    print("Modality............:", dataset.Modality)
-    print("Body Part Examined..:", dataset.BodyPartExamined)
-    print("View Position.......:", dataset.ViewPosition)
+    print("Patient id..........:", dicom_data.PatientID)
+    print("Patient's Age.......:", dicom_data.PatientAge)
+    print("Patient's Sex.......:", dicom_data.PatientSex)
+    print("Modality............:", dicom_data.Modality)
+    print("Body Part Examined..:", dicom_data.BodyPartExamined)
+    print("View Position.......:", dicom_data.ViewPosition)
     
-    if 'PixelData' in dataset:
-        rows = int(dataset.Rows)
-        cols = int(dataset.Columns)
+    if 'PixelData' in dicom_data:
+        rows = int(dicom_data.Rows)
+        cols = int(dicom_data.Columns)
         print("Image size.......: {rows:d} x {cols:d}, {size:d} bytes".format(
-            rows=rows, cols=cols, size=len(dataset.PixelData)))
-        if 'PixelSpacing' in dataset:
-            print("Pixel spacing....:", dataset.PixelSpacing)
+            rows=rows, cols=cols, size=len(dicom_data.PixelData)))
+        if 'PixelSpacing' in dicom_data:
+            print("Pixel spacing....:", dicom_data.PixelSpacing)
 
-def dicom_to_dict(dicom_data, file_path, rles_df, encoded_pixels=True):
+
+def dicom_to_dict(file_path, rles_df, encoded_pixels=True, ):
     """Parse DICOM dataset and returns a dictonary with relevant fields.
 
     Args:
-        dicom_data (dicom): chest x-ray data in dicom format.
         file_path (str): file path of the dicom data.
         rles_df (pandas.core.frame.DataFrame): Pandas dataframe of the RLE.
-        encoded_pixels (bool): if True we will search for annotation.
+        encoded_pixels (bool): Search for annotation.
         
     Returns:
         dict: contains metadata of relevant fields.
     """
     
     data = {}
-    
+    dicom_data = pydicom.dcmread(file_path)
     # Parse fields with meaningful information
     data['patient_name'] = dicom_data.PatientName
     data['patient_id'] = dicom_data.PatientID
@@ -59,12 +61,9 @@ def dicom_to_dict(dicom_data, file_path, rles_df, encoded_pixels=True):
     
     # look for annotation if enabled (train set)
     if encoded_pixels:
-        encoded_pixels_list = rles_df[rles_df['ImageId']==dicom_data.SOPInstanceUID]['EncodedPixels'].values
-       
-        pneumothorax = False
-        for encoded_pixels in encoded_pixels_list:
-            if encoded_pixels != ' -1':
-                pneumothorax = True
+        encoded_pixels = rles_df[rles_df['ImageId']==dicom_data.SOPInstanceUID]['EncodedPixels'].values
+        assert len(encoded_pixels) == 1, "Use rle-train-filtered.csv"
+        pneumothorax = False if encoded_pixels == ' -1' else True
         
         # get meaningful information (for train set)
         data['encoded_pixels_list'] = encoded_pixels_list
@@ -73,51 +72,12 @@ def dicom_to_dict(dicom_data, file_path, rles_df, encoded_pixels=True):
         
     return data
 
-def rle_to_string(runs):
-    """Array to str"""
-    return ' '.join(str(x) for x in runs)
-    
-def rle_decode(rle_str, shape, fill_value=1, dtype=int, relative=False):
-    
-    """
-    Args:
-        rle_str (str): rle string
-        shape (Tuple[int, int]): shape of the output mask
-        relative: if True, rle_str is relative encoded string
-    """
-    s = rle_str.strip().split(" ")
-    starts, lengths = np.array([np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])])
-    mask = np.zeros(np.prod(shape), dtype=dtype)
-    if relative:
-        start = 0
-        for index, length in zip(starts, lengths):
-            start = start + index
-            end = start + length
-            mask[start: end] = fill_value
-            start = end
-        return mask.reshape(shape[::-1]).T
-    else:
-        starts -= 1
-        ends = starts + lengths
-        for lo, hi in zip(starts, ends):
-            mask[lo:hi] = 1
-        return mask.reshape(shape[::-1]).T
-
-def bounding_box(img):
-    # return max and min of a mask to draw bounding box
-    rows = np.any(img, axis=1)
-    cols = np.any(img, axis=0)
-    rmin, rmax = np.where(rows)[0][[0, -1]]
-    cmin, cmax = np.where(cols)[0][[0, -1]]
-
-    return rmin, rmax, cmin, cmax
-
-def plot_with_mask_and_bbox(file_path, mask_encoded_list, figsize=(20,10), relative=True):    
-    """Plot Chest Xray image with mask(annotation or label) and without mask.
+def plot_with_mask(file_path, mask_encoded, figsize=(20,10), relative=True):    
+    """Plot Chest Xray image with mask (annotation or label) and without mask.
 
     Args:
         file_path (str): file path of the dicom data.
-        mask_encoded (numpy.ndarray): Pandas dataframe of the RLE.
+        mask_encoded : Pandas dataframe of the RLE.
         
     Returns:
         plots the image with and without mask.
@@ -131,34 +91,21 @@ def plot_with_mask_and_bbox(file_path, mask_encoded_list, figsize=(20,10), relat
     adapteq_pixel_array = exposure.equalize_adapthist(pixel_array, clip_limit=0.03)
     
     # use the masking function to decode RLE
-    mask_decoded_list = [rle_decode(mask_encoded, (1024, 1024), relative=relative) for mask_encoded in mask_encoded_list]
+    mask_decoded = rle_decode(mask_encoded, (1024, 1024), relative=relative)
     
     fig, ax = plt.subplots(nrows=1, ncols=3, sharey=True, figsize=(20,10))
     
     # print out the xray
     ax[0].imshow(pixel_array, cmap=plt.cm.bone)
-    # print the bounding box
-    for mask_decoded in mask_decoded_list:
-        # print out the annotated area
-        ax[0].imshow(mask_decoded, alpha=0.3, cmap="Reds")
-        rmin, rmax, cmin, cmax = bounding_box(mask_decoded)
-        bbox = patches.Rectangle((cmin,rmin),cmax-cmin,rmax-rmin,linewidth=1,edgecolor='r',facecolor='none')
-        ax[0].add_patch(bbox)
+    # print out the annotated area
+    ax[0].imshow(mask_decoded, alpha=0.3, cmap="Reds")
     ax[0].set_title('With Mask')
     
-    # plot image with clahe processing with just bounding box and no mask
+    # plot image with clahe processing and no mask
     ax[1].imshow(clahe_pixel_array, cmap=plt.cm.bone)
-    for mask_decoded in mask_decoded_list:
-        rmin, rmax, cmin, cmax = bounding_box(mask_decoded)
-        bbox = patches.Rectangle((cmin,rmin),cmax-cmin,rmax-rmin,linewidth=1,edgecolor='r',facecolor='none')
-        ax[1].add_patch(bbox)
     ax[1].set_title('Without Mask - Clahe')
     
-    # plot plain xray with just bounding box and no mask
+    # plot image with adapteq processing and no mask
     ax[2].imshow(adapteq_pixel_array, cmap=plt.cm.bone)
-    for mask_decoded in mask_decoded_list:
-        rmin, rmax, cmin, cmax = bounding_box(mask_decoded)
-        bbox = patches.Rectangle((cmin,rmin),cmax-cmin,rmax-rmin,linewidth=1,edgecolor='r',facecolor='none')
-        ax[2].add_patch(bbox)
     ax[2].set_title('Without Mask - Adapteq')
     plt.show()
